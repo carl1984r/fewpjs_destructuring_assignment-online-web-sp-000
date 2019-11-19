bc45da0329377584cc8515afe9e6e50e1da5e801
Bry Cox <carl.cc@gmail.com>
2019-11-19 08:43:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
-const { name, color, song, job, partner } = k_muppet.k_album.the_muppet_movie
+const { song_2, song_4 } = k_muppet.k_album.the_muppet_movie
 
 // Strings
 
