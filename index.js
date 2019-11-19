985323826a6e95434d21c576b64500097aebee67
Bry Cox <carl.cc@gmail.com>
2019-11-19 08:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const farmAnimals = 'cow horse sheep pig chicken'
-const [moo, neigh, lastName] = 'cow horse sheep pig chicken'.split(' ')
+const [moo, neigh, baa] = 'cow horse sheep pig chicken'.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
